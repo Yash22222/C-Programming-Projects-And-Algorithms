88f621acd5f72e451cff3fdfe9738ba91698b0ac
Yash Ashok Shirsath <97459174+Yash22222@users.noreply.github.com>
2023-08-13 10:00:44 +0530

    BEST FIT

--- Dynamic Partitioning Placement Algorithms/main.c
+++ Dynamic Partitioning Placement Algorithms/main.c
@@ -1,13 +1,12 @@
-//WORST FIT
+//BEST FIT
 
 #include<stdio.h>
 #include<conio.h>
 #define max 25
 void main()
 {
-int frag[max],b[max],f[max],i,j,nb,nf,temp,highest=0;
+int frag[max],b[max],f[max],i,j,nb,nf,temp,lowest=10000;
 static int bf[max],ff[max];
-printf("\n\tMemory Management Scheme - Worst Fit");
 printf("\nEnter the number of blocks:");
 scanf("%d",&nb);
 printf("Enter the number of files:");
@@ -26,26 +25,26 @@ scanf("%d",&f[i]);
 }
 for(i=1;i<=nf;i++)
 {
-
 for(j=1;j<=nb;j++)
 {
-if(bf[j]!=1) //if bf[j] is not allocated
+if(bf[j]!=1)
 {
 temp=b[j]-f[i];
 if(temp>=0)
-if(highest<temp)
+if(lowest>temp)
 {
 ff[i]=j;
-highest=temp;
+
+lowest=temp;
 }
 }
 }
-frag[i]=highest;
+frag[i]=lowest;
 bf[ff[i]]=1;
-highest=0;
+lowest=10000;
 }
-printf("\nFile_no:\tFile_size :\tBlock_no:\tBlock_size:\tFragement");
-for(i=1;i<=nf;i++)
+printf("\nFile No\tFile Size \tBlock No\tBlock Size\tFragment");
+for(i=1;i<=nf && ff[i]!=0;i++)
 printf("\n%d\t\t%d\t\t%d\t\t%d\t\t%d",i,f[i],ff[i],b[ff[i]],frag[i]);
 getch();
 }
